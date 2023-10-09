ccb19cb4584f8f86d37f00eeb9cf1ffbb13b1e4b
Mannuel F. Granoble Pino <57782912+pinomannuel26@users.noreply.github.com>
2023-10-09 11:41:23 -0500

    crew

---
+++ src/components/SliderCrew/SliderCrew.jsx
@@ -0,0 +1,15 @@
+
+export const SliderCrew =[
+    {
+        id:1, workload:'commander', name:'Douglas Hurley', description:'Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.',img:'/ImgCrew/image-removebg-preview(289).png'
+    },
+    {
+        id:2, workload:'Mission Specialist', name:'MARK SHUTTLEWORTH', description:'Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.',img:'/ImgCrew/image-removebg-preview(262).png'
+    },
+    {
+        id:3, workload:'PILOT', name:'Victor Glover', description:'Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.',img:'/ImgCrew/image-removebg-preview(288).png'
+    },
+    {
+        id:4, workload:'Flight Engineer', name:'Anousheh Ansari', description:'Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.',img:'/ImgCrew/image-removebg-preview(265).png'
+    }
+]
--- src/views/Crew/Crew.jsx
+++ src/views/Crew/Crew.jsx
@@ -1,5 +1,226 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { SliderCrew } from "../../components/SliderCrew/SliderCrew";
+
+const DivPrincipal = styled.div`
+  width: 100%;
+  height: 900px;
+  background-image: url("/ImgCrew/Bitmap.png");
+  background-size: 100% 100%;
+`;
+
+const DivContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  padding-left: 12rem;
+  @media screen and (max-width: 500px) {
+    padding-left: 0;
+  }
+  @media screen and (min-width: 501px) and (max-width: 1023px) {
+    padding-left: 2.5rem;
+  }
+`;
+const DivTextImg = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  @media screen and (max-width: 500px) {
+    flex-direction: column-reverse;
+    justify-content: center;
+    align-items: center;
+    height: 80%;
+  }
+  @media screen and (min-width: 501px) and (max-width: 1023px) {
+    flex-direction: column;
+    align-items: center;
+    width: 90%;
+    height: auto;
+  }
+`;
+
+const H2 = styled.h2`
+  color: white;
+  font-size: 28px;
+  font-family: "Barlow", sans-serif;
+  font-weight: 400;
+  word-wrap: break-word;
+  padding-top: 15rem;
+
+  @media screen and (max-width: 500px) {
+    font-size: 16px;
+    padding-top: 6rem;
+    display: flex;
+    justify-content: center;
+    letter-spacing: 2.7px;
+    font-family: "Barlow Condensed", sans-serif;
+  }
+  @media screen and (min-width: 501px) and (max-width: 1023px) {
+    padding-top: 10rem;
+  }
+`;
+const H2span = styled.span`
+  font-weight: 700;
+  opacity: 0.4;
+  @media screen and (max-width: 500px) {
+    font-size: 17px;
+    font-weight: 400;
+    font-family: "Barlow Condensed", sans-serif;
+  }
+`;
+
+const DivText = styled.div`
+  color: white;
+  word-wrap: break-word;
+
+  @media screen and (max-width: 500px) {
+    width: 88%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-top: 2px solid #383b4b;
+  }
+  @media screen and (min-width: 501px) and (max-width: 1023px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+const DivTextH3 = styled.h3`
+  font-size: 32px;
+  font-family: "Bellefair", serif;
+  font-weight: 400;
+  text-transform: upperCase;
+  opacity: 0.4;
+  margin-top: 6rem;
+  margin-bottom: 0;
+  @media screen and (max-width: 500px) {
+    font-size: 16px;
+    margin-top: 2rem;
+  }
+  @media screen and (min-width: 501px) and (max-width: 1024px) {
+    font-size: 24px;
+    margin-top: 4rem;
+  }
+`;
+const DivTextH2 = styled.h2`
+  font-size: 56px;
+  font-family: "Bellefair", serif;
+  font-weight: 400;
+  text-transform: upperCase;
+  width: 40rem;
+  margin-top: 1.5rem;
+  @media screen and (max-width: 500px) {
+    font-size: 24px;
+    width: auto;
+    margin-top: 1rem;
+  }
+  @media screen and (min-width: 501px) and (max-width: 1024px) {
+    width: auto;
+    font-size: 40px;
+    margin-top: 0.5rem;
+    margin-bottom: 1rem;
+  }
+`;
+const DivTextP = styled.p`
+  width: 28rem;
+  height: 15rem;
+  color: #d0d6f9;
+  font-size: 18px;
+  font-family: "Barlow", sans-serif;
+  font-weight: 400;
+  line-height: 32px;
+  @media screen and (max-width: 500px) {
+    display: flex;
+    text-align: center;
+    width: 100%;
+    height: 8rem;
+    font-size: 15px;
+    line-height: 25px;
+  }
+  @media screen and ((min-width: 501px)and (max-width:1023px)) {
+    font-size: 16px;
+    line-height: 28px;
+    width: 80%;
+    border: 1px solid blue;
+    text-align: center;
+    margin-top: 0;
+    margin-bottom: 2rem;
+    height: auto;
+  }
+`;
+const DivImg = styled.img`
+  width: 30%;
+  height: 70%;
+  margin-top: -2rem;
+  @media screen and (max-width: 500px) {
+    width: 50%;
+    height: 35%;
+    margin-top: 0;
+  }
+  @media screen and ((min-width: 501px)and (max-width:1023px)) {
+    width: 50%;
+    max-height: 60vh;
+    margin-top: 1rem;
+  }
+`;
+
+const DivSlider = styled.div`
+  display: flex;
+  @media screen and (max-width: 500px) {
+    margin-top: 2rem;
+    order: -1;
+  }
+  @media screen and ((min-width: 501px)and (max-width:1023px)) {
+    margin-top: 0;
+  }
+`;
+const SliderBottomOpacy = styled.div`
+  width: 0.8rem;
+  height: 0.8rem;
+  background-color: white;
+  border-radius: 10px;
+  margin-right: 1rem;
+  opacity: 0.4;
+`;
+const SliderBottom = styled.div`
+  width: 0.8rem;
+  height: 0.8rem;
+  background-color: white;
+  border-radius: 10px;
+  margin-right: 1rem;
+`;
+
 const Crew = () => {
-  return <div>Crew</div>;
+  const [imgIndex, setImIndex] = useState(0);
+  const goToSlide = (slideIndex) => {
+    setImIndex(slideIndex);
+  };
+  return (
+    <DivPrincipal>
+      <DivContainer>
+        <H2>
+          <H2span>02</H2span> MEET YOUR CREW
+        </H2>
+        <DivTextImg>
+          <DivText>
+            <DivTextH3>{SliderCrew[imgIndex].workload}</DivTextH3>
+            <DivTextH2>{SliderCrew[imgIndex].name}</DivTextH2>
+            <DivTextP>{SliderCrew[imgIndex].description}</DivTextP>
+
+            <DivSlider>
+              {SliderCrew.map((_, idx) => (
+                <div key={idx} onClick={() => goToSlide(idx)}>
+                  {imgIndex === idx ? <SliderBottom /> : <SliderBottomOpacy />}
+                </div>
+              ))}
+            </DivSlider>
+          </DivText>
+
+          <DivImg src={SliderCrew[imgIndex].img} alt="img" />
+        </DivTextImg>
+      </DivContainer>
+    </DivPrincipal>
+  );
 };
 
 export default Crew;
