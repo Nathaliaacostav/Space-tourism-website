21cbcfb432f920eeac06b57e8119424f2bc89d05
Santiago Muñoz <josmansant@unicauca.edu.co>
2023-10-08 22:45:56 -0500

    Technology for desktop and table

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
-import Crew from "./views/Crew/Crew";
 import Page404 from "./views/Page404/Page404";
 import Home from "./views/Home";
 import Destination from "./views/Destination";
+import TechnologyContent from "./components/TechnologyContent";
 import "./sass/global.scss";
 
 const AppRoutes = () => {
@@ -13,8 +13,7 @@ const AppRoutes = () => {
         <Route index element={<Home />} />
         <Route path="home" element={<Home />} />
         <Route path="destination" element={<Destination />} />
-        <Route path="crew" element={<Crew />} />
-        <Route path="technology" element={<Crew />} />
+        <Route path="technology" element={<TechnologyContent />} />
         <Route path="*" element={<Page404 />} />
       </Route>
     </Routes>
