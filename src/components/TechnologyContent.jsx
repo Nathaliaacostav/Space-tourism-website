d009030eb3ecdd127f4e26bb4bf215d66532f700
Santiago Muñoz <josmansant@unicauca.edu.co>
2023-10-08 22:45:56 -0500

    Technology for desktop and table

--- src/components/TechnologyContent.jsx
+++ src/components/TechnologyContent.jsx
@@ -317,6 +317,230 @@ const SvgTable = styled.svg`
   }
 `;
 
+const customMedia = generateMedia({
+  desktop: "1440px",
+  tablet: "1024px",
+});
+
+const DivContainer = styled.div`
+  width: 1440px;
+  height: 900px;
+  background-color: #0b0d17;
+  background-image: url("/public/backgrounTechnology.png");
+
+  ${customMedia.lessThan("tablet")`
+      width: 768px;
+      height: 1024px;
+      
+  `};
+`;
+const DivContainerTechno = styled.div`
+  display: flex;
+  padding-top: 100px;
+  margin-left: 167px;
+  gap: 24px;
+
+  ${customMedia.lessThan("tablet")`
+        position: relative;
+        right: 130px;
+        top: -50px;
+    `};
+`;
+const PtextNum = styled.p`
+  color: #fff;
+  font-family: Barlow Condensed;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: 4.725px;
+  opacity: 0.25;
+
+  ${customMedia.lessThan("tablet")`
+        color: #FFF;
+        font-family: Barlow Condensed;
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: normal;
+        letter-spacing: 3.375px;  
+`};
+`;
+const PtextTitle = styled.p`
+  color: #fff;
+  font-family: Barlow Condensed;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 4.725px;
+  ${customMedia.lessThan("tablet")`
+      color: #FFF;
+      font-family: Barlow Condensed;
+      font-size: 20px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+      letter-spacing: 3.375px;
+      text-transform: uppercase;
+    `};
+`;
+const DivSlider = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-left: 140px;
+
+  ${customMedia.lessThan("tablet")`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: relative;
+    cursor: pointer;
+  `};
+`;
+const Divbuttons = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: relative;
+  cursor: pointer;
+
+  ${customMedia.lessThan("tablet")`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    top: 300px;
+    right: 70px;
+    cursor: pointer;
+  `};
+`;
+const DivbuttonsItem = styled.div`
+  margin: 10px;
+`;
+const DivContentSlider = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  ${customMedia.lessThan("tablet")`
+    display: flex;
+    width: 458px;
+    flex-direction: column-reverse;
+    justify-content: center;
+    align-items: center;
+    gap: 16px;
+  `};
+`;
+const Divtext = styled.div`
+  width: 500px;
+  height: 271px;
+  flex-shrink: 0;
+  margin-right: 80px;
+  margin-left: 80px;
+  ${customMedia.lessThan("tablet")`
+      display: flex;
+      width: 458px;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      gap: 16px;
+    `};
+`;
+const DivOnetext = styled.div`
+  color: #d0d6f9;
+  font-family: Barlow Condensed;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 2.7px;
+  ${customMedia.lessThan("tablet")`´
+          display: flex;      
+          position: relative;
+          top: 50px;
+          color: #D0D6F9;
+          font-family: Barlow Condensed;
+          font-size: 16px;
+          font-style: normal;
+          font-weight: 400;
+          line-height: normal;
+          letter-spacing: 2.7px;
+    `};
+`;
+const Divcontent = styled.div`
+  color: #d0d6f9;
+  font-family: Barlow;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 32px;
+  ${customMedia.lessThan("tablet")`
+          color: #D0D6F9;
+          text-align: center;
+          font-family: Barlow;
+          font-size: 16px;
+          font-style: normal;
+          font-weight: 400;
+          line-height: 28px; /* 175% */
+      `};
+`;
+
+const DivSecondText = styled.div`
+  color: #fff;
+  font-family: Bellefair;
+  font-size: 56px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+
+  ${customMedia.lessThan("tablet")`
+      color: #FFF;
+      text-align: center;
+      font-family: Bellefair;
+      font-size: 40px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+      text-transform: uppercase;
+    `};
+`;
+const DivImg = styled.div`
+  ${customMedia.lessThan("tablet")`
+      width: 768px;
+      height: 310px;
+      position: relative;
+      top: -110px;
+    `};
+`;
+const Imgdesktop = styled.img`
+  display: bock;
+  ${customMedia.lessThan("tablet")`
+      display: none;
+    `};
+`;
+const ImgTable = styled.img`
+  display: none;
+  ${customMedia.lessThan("tablet")`
+      display: block;
+    `};
+`;
+const SvgDesktop = styled.svg`
+  display: block;
+  ${customMedia.lessThan("tablet")`
+      display: none;
+  `};
+`;
+
+const SvgTable = styled.svg`
+  display: none;
+  ${customMedia.lessThan("tablet")`
+      display: block;
+  `};
+`;
+
 function TechnologyContent() {
   const items = [
     {
